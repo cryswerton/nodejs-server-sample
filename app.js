bff963444604684ea66d4dac7b38906ac1fdf6e4
cryswerton <cryswertons@gmail.com>
2021-02-03 22:11:01 -0300

    added index.html to be served by the server

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const http = require('http');
+const fs = require('fs')
 
 const hostname = '10.0.0.108';
 const port = 3000;
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader('content-type', 'text/html');
+  fs.createReadStream('index.html').pipe(res)
 });
 
 app.listen(port, hostname, () => {
